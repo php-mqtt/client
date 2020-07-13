76354fc622241160056d81fc1a0d6d83ef45a612
Marvin Mall <marvin-mall@msn.com>
2020-07-13 20:54:57 +0200

    Ensure client is connected before performing actions

---
+++ src/Exceptions/ClientNotConnectedToBrokerException.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpMqtt\Client\Exceptions;
+
+/**
+ * Exception to be thrown if an MQTT client is not connected to a broker and tries
+ * to perform an action which requires a connection (e.g. publish or subscribe).
+ */
+class ClientNotConnectedToBrokerException extends DataTransferException
+{
+}
