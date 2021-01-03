f5697f0d62ebeefe8b626853faada1898667c4d7
Marvin Mall <marvin-mall@msn.com>
2021-01-03 10:38:34 +0100

    Add tests for connection settings validation

--- src/Concerns/ValidatesConfiguration.php
+++ src/Concerns/ValidatesConfiguration.php
@@ -39,8 +39,8 @@ trait ValidatesConfiguration
             throw new ConfigurationInvalidException('The resend timeout cannot be less than 1 second.');
         }
 
-        if ($settings->getKeepAliveInterval() < 1 || $settings->getKeepAliveInterval() > 65536) {
-            throw new ConfigurationInvalidException('The keep alive interval must be a value in the range of 1 to 65536 seconds.');
+        if ($settings->getKeepAliveInterval() < 1 || $settings->getKeepAliveInterval() > 65535) {
+            throw new ConfigurationInvalidException('The keep alive interval must be a value in the range of 1 to 65535 seconds.');
         }
 
         if ($settings->getUsername() !== null && trim($settings->getUsername()) === '') {
