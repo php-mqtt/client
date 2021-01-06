7ba291f24a16835fa6227c3ad7bae4d97727eb2e
Marvin Mall <marvin-mall@msn.com>
2021-01-06 20:53:36 +0100

    Try to run TLS tests using Mosquitto

---
+++ .github/actions/mosquitto-mqtt-broker/Dockerfile
@@ -0,0 +1,7 @@
+FROM docker:stable
+
+COPY entrypoint.sh /entrypoint.sh
+
+RUN chmod +x /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ .github/actions/mosquitto-mqtt-broker/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+docker_run="docker run --detach --name mosquitto"
+
+for i in $(echo $INPUT_PORTS | tr " " "\n")
+do
+  docker_run="$docker_run --publish $i"
+done
+
+if [ -n "$INPUT_CERTIFICATES" ]; then
+  docker_run="$docker_run --volume $GITHUB_WORKSPACE/$INPUT_CERTIFICATES:/mosquitto-certs"
+fi
+
+if [ -n "$INPUT_CONFIG" ]; then
+  docker_run="$docker_run --volume $GITHUB_WORKSPACE/$INPUT_CONFIG:/mosquitto/config/mosquitto.conf"
+fi
+
+docker_run="$docker_run eclipse-mosquitto:$INPUT_VERSION"
+
+sh -c "$docker_run"
