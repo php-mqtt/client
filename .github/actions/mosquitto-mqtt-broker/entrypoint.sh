dded35b42b48b517b54884a714e67b5d18eb1063
Marvin Mall <marvin-mall@msn.com>
2021-01-06 21:46:25 +0100

    Try custom directory for mosquitto config

--- .github/actions/mosquitto-mqtt-broker/entrypoint.sh
+++ .github/actions/mosquitto-mqtt-broker/entrypoint.sh
@@ -12,10 +12,14 @@ if [ -n "$INPUT_CERTIFICATES" ]; then
 fi
 
 if [ -n "$INPUT_CONFIG" ]; then
-  docker_run="$docker_run --volume $GITHUB_WORKSPACE/$INPUT_CONFIG:/mosquitto/config/mosquitto.conf:ro"
+  docker_run="$docker_run --volume $GITHUB_WORKSPACE/$INPUT_CONFIG:/mosquitto-config:ro"
 fi
 
 docker_run="$docker_run eclipse-mosquitto:$INPUT_VERSION"
+
+if [ -n "$INPUT_CONFIG" ]; then
+  docker_run="$docker_run /usr/sbin/mosquitto -c /mosquitto-config/mosquitto.conf"
+fi
 
 echo "$docker_run"
 sh -c "$docker_run"
