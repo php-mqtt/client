e07fb60bd4152eda3df3fe53308c3b7196be36a9
Marvin Mall <marvin-mall@msn.com>
2021-01-06 23:01:27 +0100

    Revert "Try custom directory for mosquitto config"
    This reverts commit dded35b42b48b517b54884a714e67b5d18eb1063.

--- .github/actions/mosquitto-mqtt-broker/entrypoint.sh
+++ .github/actions/mosquitto-mqtt-broker/entrypoint.sh
@@ -12,14 +12,10 @@ if [ -n "$INPUT_CERTIFICATES" ]; then
 fi
 
 if [ -n "$INPUT_CONFIG" ]; then
-  docker_run="$docker_run --volume $GITHUB_WORKSPACE/$INPUT_CONFIG:/mosquitto-config:ro"
+  docker_run="$docker_run --volume $GITHUB_WORKSPACE/$INPUT_CONFIG:/mosquitto/config/mosquitto.conf:ro"
 fi
 
 docker_run="$docker_run eclipse-mosquitto:$INPUT_VERSION"
-
-if [ -n "$INPUT_CONFIG" ]; then
-  docker_run="$docker_run /usr/sbin/mosquitto -c /mosquitto-config/mosquitto.conf"
-fi
 
 echo "$docker_run"
 sh -c "$docker_run"
