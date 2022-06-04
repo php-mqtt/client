77f68543540bee56a61669bf211b3e4d5afa0c4d
Namoshek <Namoshek@users.noreply.github.com>
2022-06-04 10:56:32 +0200

    Run some integration tests with authentication (#113)
    * Support running some tests with authentication
    
    * Enable authentication for Mosquitto
    
    * Use authentication only with Mosquitto for now
    
    * Use password-file with mosquitto-github-action
    
    * Fix mosquitto.passwd

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,8 +13,12 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
     protected string $mqttBrokerHost;
     protected int $mqttBrokerPort;
+    protected int $mqttBrokerPortWithAuthentication;
     protected int $mqttBrokerTlsPort;
     protected int $mqttBrokerTlsWithClientCertificatePort;
+
+    protected ?string $mqttBrokerUsername = null;
+    protected ?string $mqttBrokerPassword = null;
 
     protected bool $skipTlsTests;
     protected string $tlsCertificateDirectory;
@@ -28,8 +32,12 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
 
         $this->mqttBrokerHost                         = getenv('MQTT_BROKER_HOST');
         $this->mqttBrokerPort                         = intval(getenv('MQTT_BROKER_PORT'));
+        $this->mqttBrokerPortWithAuthentication       = intval(getenv('MQTT_BROKER_PORT_WITH_AUTHENTICATION'));
         $this->mqttBrokerTlsPort                      = intval(getenv('MQTT_BROKER_TLS_PORT'));
         $this->mqttBrokerTlsWithClientCertificatePort = intval(getenv('MQTT_BROKER_TLS_WITH_CLIENT_CERT_PORT'));
+
+        $this->mqttBrokerUsername                     = getenv('MQTT_BROKER_USERNAME') ?: null;
+        $this->mqttBrokerPassword                     = getenv('MQTT_BROKER_PASSWORD') ?: null;
 
         $this->skipTlsTests            = getenv('SKIP_TLS_TESTS') === 'true';
         $this->tlsCertificateDirectory = rtrim(getenv('TLS_CERT_DIR'), '/');
