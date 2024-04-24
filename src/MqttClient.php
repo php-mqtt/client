6f3404db9879daacae4ca8b09700c6ab0bcfee30
André <andre.feld@anrath.net>
2024-04-24 13:05:35 +0200

    Update MqttClient.php
    Added TLS ALPN option to the TLS options

--- src/MqttClient.php
+++ src/MqttClient.php
@@ -197,6 +197,9 @@ class MqttClient implements ClientContract
                 $tlsOptions['passphrase'] = $this->settings->getTlsClientCertificateKeyPassphrase();
             }
 
+            if ($this->settings->getTlsAlpn() !== null) {
+                $tlsOptions['alpn_protocols'] = $this->settings->getTlsAlpn();
+ 
             $contextOptions['ssl'] = $tlsOptions;
         }
 
